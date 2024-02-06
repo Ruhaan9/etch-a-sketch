8505e31438d6e7c791f955126129a42dbefadec7
Ruhaan <ruhaan313@gmail.com>
2024-02-06 21:27:31 +0500

    Add on hover color-change function

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ mainContainer = document.querySelector('#container');
 
 for (let i = 0; i < 2000; i++) {
     let grid = document.createElement('div');
-    grid.style.backgroundColor = 'blue';
     grid.style.padding = '10px';
+    grid.style.border = '1px solid black';
     mainContainer.appendChild(grid);
-}
+};
+
+let grid = mainContainer.querySelectorAll('*');
+
+
+    grid.forEach(function(node) {
+    node.addEventListener('mouseenter', function() {
+      node.style.backgroundColor = 'black';
+    });
+  });
+  
