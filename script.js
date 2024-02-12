b93f5107dc792dbff919c68a479d31647f593ec0
Ruhaan <ruhaan313@gmail.com>
2024-02-12 13:38:52 +0500

    Save Progress

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 mainContainer = document.querySelector('#container');
 
-for (let i = 0; i < 2000; i++) {
+let sizeBtn = document.querySelector('#gridPrompt');
+let gridSize = 16;
+
+sizeBtn.addEventListener('click', () => {
+    
+    let size = '';
+    while ( size == '' || size < 0 || size > 100)  {
+    size = prompt("Enter the Grid Size between 1 and 100", "16");
+    };
+})
+
+
+let elementSize = ((700/gridSize)/2) + 'px';
+
+for (let i = 0; i < gridSize*gridSize; i++) {
     let grid = document.createElement('div');
-    grid.style.padding = '10px';
+    grid.style.padding = '20px';
     grid.style.border = '1px solid black';
     mainContainer.appendChild(grid);
 };
 
-let grid = mainContainer.querySelectorAll('*');
+    let grid = mainContainer.querySelectorAll('*');
 
 
     grid.forEach(function(node) {
@@ -15,4 +29,6 @@ let grid = mainContainer.querySelectorAll('*');
       node.style.backgroundColor = 'black';
     });
   });
+
+  
   
